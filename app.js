15adc0f7da46e36f43ba43449baeecbcf9c58884
gavic007 <mylesgavic@gmail.com>
2015-01-29 12:58:58 -0500

    Added picture of a bear

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.use(express.static('bower_components'));
 
 app.get('/kittens', function(req, res){
     res.sendFile('public/views/kittens.html', options);
-    res.send("Page under construction!");
 });
 
 app.get('/about', function(req, res){
